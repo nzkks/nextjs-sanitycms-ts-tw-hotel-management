eef4e208fbc1f1d958528a8f49295605ad6d3999
nzkks <kkshanthosh@gmail.com>
2024-05-20 12:51:41 +1200

    CMS: Add booking, hotel room, review schemas

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -1,4 +1,7 @@
 import account from './account';
+import booking from './booking';
+import hotelRoom from './hotelRoom';
+import review from './review';
 import user from './user';
 
-export const schemaTypes = [account, user];
+export const schemaTypes = [account, booking, hotelRoom, review, user];
