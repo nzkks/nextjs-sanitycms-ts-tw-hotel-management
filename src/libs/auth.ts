6fc8eca3cda2843a6c33bfe53afb1a03ef932bb1
nzkks <kkshanthosh@gmail.com>
2024-05-21 12:06:05 +1200

    auth.ts - Google provider: Add allowDangerousEmailAccountLinking flag to avoid OAuthAccountNotLinked Error

--- src/libs/auth.ts
+++ src/libs/auth.ts
@@ -14,6 +14,7 @@ export const authOptions: NextAuthOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      allowDangerousEmailAccountLinking: true,
     }),
     SanityCredentials(sanityClient),
   ],
