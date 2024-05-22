12abc5826972cac4d69b431ade397b2f602e7fae
nzkks <kkshanthosh@gmail.com>
2024-05-22 13:23:09 +1200

    Show an error when something goes wrong like fetch didn't work, etc

---
+++ src/app/(web)/error.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  return (
+    <div className="container mx-auto">
+      <h2 className="font-heading mb-10 text-red-800">Something went wrong!</h2>
+
+      <button onClick={() => reset()} className="btn-primary">
+        Try Again
+      </button>
+    </div>
+  );
+}
