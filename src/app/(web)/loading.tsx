46d1d5e2a09e2413b56f6e11f1a8fec71e58784d
nzkks <kkshanthosh@gmail.com>
2024-05-22 13:14:32 +1200

    Show loading spinner on loading a page

---
+++ src/app/(web)/loading.tsx
@@ -0,0 +1,7 @@
+const LoadingSpinner = () => (
+  <div className="flex h-screen items-center justify-center">
+    <div className="h-16 w-16 animate-spin rounded-full border-b-2 border-t-2 border-gray-900" />
+  </div>
+);
+
+export default LoadingSpinner;
