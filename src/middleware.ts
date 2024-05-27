3c598fbb99ae28a74a138bd8ddcdb009b4283e11
nzkks <kkshanthosh@gmail.com>
2024-05-27 14:06:45 +1200

    middleware.ts: fix the paths

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,5 @@
 export { default } from 'next-auth/middleware';
 
 export const config = {
-  matcher: ['/users/:path*'],
+  matcher: ['/users/:path*', '/((?!api|_next/static|_next/image|.*\\.png$).*)'],
 };
